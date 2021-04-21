f9519fb9ba63ba59124c968edf82d3b0699b47a0
AkiKagura <49899720+AkiKagura@users.noreply.github.com>
2021-04-21 18:08:50 +0800

    Update main.py

--- PyWin32Word/main.py
+++ PyWin32Word/main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     app = win32com.client.Dispatch('Word.Application')
     app.Visible = 0  # 后台运行
     app.DisplayAlerts = 0  # 不显示，不警告
-    doc = app.Documents.Open('D:\\MES0205\\01_doc\\05_実装段階\\PPA\\省エネ最適化_取扱説明書_1.2.3版_横.docx')   # 打开一个已有的word文档
+    doc = app.Documents.Open('D:\\MES0205\\01_doc\\1\\1\\1.docx')   # 打开一个已有的word文档
     # new_doc = word.Documents.Add() # 创建新的word文档
 
     # read
